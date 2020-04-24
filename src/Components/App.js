cfe6cae7441ee271cb12047546a7746f6e46664c
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-24 15:33:21 -0700

    Add KegControl link

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import '../App.css';
 import Header from './Shared/Header'
 import Footer from './Shared/Footer'
+import KegControl from './KegControl'
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <KegControl />
       <Footer />
     </React.Fragment>
   );
