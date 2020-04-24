b5dc2fb5aedbb05032e066669ce623da56f58a72
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-24 15:34:22 -0700

    Add form to add a keg

---
+++ src/Components/KegStock/NewKegForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import ReusableForm from '../Shared/ResuableForm';
+
+function NewKegForm(props) {
+   
+    function handleNewKegFormSubmission(event) {
+      event.preventDefault();
+      props.onNewKegCreation({name: event.target.name.value, description: event.target.description.value, quantity: parseInt(event.target.quantity.value), id: v4()});
+    }
+  
+  return (
+    <React.Fragment>
+
+      <ReusableForm 
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add New Keg" />
+        
+    </ React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
