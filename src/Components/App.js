cbc1fc574de6da5bf88d739b25c944fd58273343
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-24 11:38:16 -0700

    Moved file to Components and changed links as appropriate.

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Header from './Shared/Header'
+import Footer from './Shared/Footer'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
