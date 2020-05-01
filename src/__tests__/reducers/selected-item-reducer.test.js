9cf57d2c78d5aae5360dcd5f10313ae453f19c5b
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-01 14:48:29 -0700

    refactor tests and code. Pass

--- src/__tests__/reducers/selected-item-reducer.test.js
+++ src/__tests__/reducers/selected-item-reducer.test.js
@@ -3,10 +3,12 @@ import selectedKegReducer from '../../reducers/selected-keg-reducer';
 describe('selectedKegReducer', () => {
   let action;
   const selectedKeg = {
-      name: 't-shirt',
-      description: '100% hemp',
-      quantity: 5,
-      id: 1 
+    name: 'Dr Xenon Bloom Stout',
+    description: 'A bold and rich stout',
+    brewery: 'Mini Morty Brewing',
+    price: '$120',
+    quantity: '124',
+    id: 1
     };
 
   test('Should return default state if no action type is recognized', () => {
@@ -16,10 +18,12 @@ describe('selectedKegReducer', () => {
   test('Should successfully set selectedKeg to the keg passed to the reducer', () => {
     action = {
       type: 'CHANGE_SELECTED',
-      name: 't-shirt',
-      description: '100% hemp',
-      quantity: 5,
-      id: 1 
+      name: 'Dr Xenon Bloom Stout',
+      description: 'A bold and rich stout',
+      brewery: 'Mini Morty Brewing',
+      price: '$120',
+      quantity: '124',
+      id: 1
     };
     expect(selectedKegReducer(null, action)).toEqual(selectedKeg);
   });
