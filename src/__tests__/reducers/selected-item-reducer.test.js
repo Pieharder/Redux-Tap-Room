ee6da2dd33175580da545c6331028d09b116f4fe
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-01 14:44:17 -0700

    Add second test to selected keg reducer

--- src/__tests__/reducers/selected-item-reducer.test.js
+++ src/__tests__/reducers/selected-item-reducer.test.js
@@ -12,4 +12,15 @@ describe('selectedKegReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(selectedKegReducer(null, { type: null })).toEqual(null);
   });
+  
+  test('Should successfully set selectedKeg to the keg passed to the reducer', () => {
+    action = {
+      type: 'CHANGE_SELECTED',
+      name: 't-shirt',
+      description: '100% hemp',
+      quantity: 5,
+      id: 1 
+    };
+    expect(selectedKegReducer(null, action)).toEqual(selectedKeg);
+  });
 });
