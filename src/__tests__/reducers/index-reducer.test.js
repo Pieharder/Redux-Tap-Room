98490f9960ef353c81b92942f3acffd742b2a70d
Geoff Goetz <geoff.goetz503@gmail.com>
2020-05-01 14:29:42 -0700

    Add additional tests to root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,12 +1,47 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'Dr Xenon Bloom Stout',
+      description: 'A bold and rich stout',
+      brewery: 'Mini Morty Brewing',
+      price: '$120',
+      quantity: '124',
+      id: 1 
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
 
 });
